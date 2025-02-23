7ed9fd96693fa6edc9725269a0af0b95889d6b38
John S Weeks <steve@offthekitchen.com>
2025-02-23 16:28:09 -0700

    Adding Base directories for admin and public files

---
+++ admin/class-steves-bike-maintenance-admin.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Steves_Bike_Maintenance
+ * @subpackage Steves_Bike_Maintenance/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Steves_Bike_Maintenance
+ * @subpackage Steves_Bike_Maintenance/admin
+ * @author     Your Name <email@example.com>
+ */
+class Steves_Bike_Maintenance_Admin {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $Steves_Bike_Maintenance    The ID of this plugin.
+	 */
+	private $Steves_Bike_Maintenance;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $Steves_Bike_Maintenance       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $Steves_Bike_Maintenance, $version ) {
+
+		$this->Steves_Bike_Maintenance = $Steves_Bike_Maintenance;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Steves_Bike_Maintenance_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Steves_Bike_Maintenance_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->Steves_Bike_Maintenance, plugin_dir_url( __FILE__ ) . 'css/plugin-name-admin.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Steves_Bike_Maintenance_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Steves_Bike_Maintenance_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->Steves_Bike_Maintenance, plugin_dir_url( __FILE__ ) . 'js/plugin-name-admin.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
---
+++ admin/partials/steves-bike-maintenance-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://www.offthekitchen.com
+ * @since      1.0.0
+ *
+ * @package    Steves_Bike_Maintenance
+ * @subpackage Steves_Bike_Maintenance/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
